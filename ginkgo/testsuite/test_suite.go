b2308e4959fe58c7bae753d693e05337d523a1c1
James Lawrence <jlawrence@datto.com>
2016-04-07 20:17:51 -0400

    skip vendor directory unless GO15VENDOREXPERIMENT is disabled

--- ginkgo/testsuite/test_suite.go
+++ ginkgo/testsuite/test_suite.go
@@ -50,10 +50,12 @@ func SuitesInDir(dir string, recurse bool) []TestSuite {
 	suites := []TestSuite{}
 
 	// "This change will only be enabled if the go command is run with
-	// GO15VENDOREXPERIMENT=1 in its environment."
+	// GO15VENDOREXPERIMENT=0 in its environment."
 	// c.f. the vendor-experiment proposal https://goo.gl/2ucMeC
+	// in 1.6 the vendor directory became the default go behaviour, so now
+	// check if its disabled.
 	vendorExperiment := os.Getenv("GO15VENDOREXPERIMENT")
-	if (vendorExperiment == "1") && path.Base(dir) == "vendor" {
+	if (vendorExperiment != "0") && path.Base(dir) == "vendor" {
 		return suites
 	}
 
